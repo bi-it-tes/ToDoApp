3f051295cb18345f19b7fbadfdbbaf92fd08deda
bi-it-tes <sabina.teleskumar@espas.ch>
2025-05-05 11:53:46 +0200

    Trying to implement the update button/function

--- Model/ToDoItem.cs
+++ Model/ToDoItem.cs
@@ -4,8 +4,9 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
         public bool IsDone { get; set; }
+
+        public bool IsEditable { get; set; } = false; 
 
     }
 }
