92180cfa0aa3b275e8c78b7e37341120c16f3065
bi-it-tes <sabina.teleskumar@espas.ch>
2025-05-05 14:19:15 +0200

    For readonly or not readonly function -UpdateButton

--- Model/ToDoItem.cs
+++ Model/ToDoItem.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public bool IsDone { get; set; }
-
         public bool IsEditable { get; set; } = false; 
 
     }
